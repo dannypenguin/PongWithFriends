94f720afeba14c2d2c217c43915d866f1c5ec12c
dannypenguin <DannyPengXD@gmail.com>
2017-02-11 17:33:41 -0800

    Accelerometer for speed fixed

--- GameScene.swift
+++ GameScene.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 import FirebaseDatabase
 import CoreMotion
+import AudioToolbox
 
 class GameScene: ViewController {
     
@@ -21,6 +22,15 @@ class GameScene: ViewController {
     
     var LabelText = String()
     var motionManager: CMMotionManager!
+    var lastupdated: NSDate = NSDate(timeIntervalSinceReferenceDate: 0)
+    var lastX: Float = 0
+    var lastY: Float = 0
+    var lastZ: Float = 0
+    var changeinVar = Float()
+    var grabFlag: Bool = true
+    var hasSwung: Bool = false
+    let shakeThreshold: Float = 2
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,14 +47,43 @@ class GameScene: ViewController {
         
         self.recordChange()
     }
+    func thresholdCipher(value: Float) {
+        if value <= 7 {
+            self.typeOfPassLabel.text = ""
+        }
+    }
     
     func recordChange(){
-
         DispatchQueue.global(qos: .background).async {
-            while true {
+            while self.grabFlag == true {
+                let start: NSDate = NSDate()
+                var cont = start.timeIntervalSince(self.lastupdated as Date)
                 if self.motionManager.accelerometerData != nil {
-                    var accelX: Double = (self.motionManager.accelerometerData?.acceleration.x)!
-                    print(accelX)
+                    var accelX: Float = Float((self.motionManager.accelerometerData?.acceleration.x)!)
+                    var accelY: Float = Float((self.motionManager.accelerometerData?.acceleration.y)!)
+                    var accelZ: Float = Float((self.motionManager.accelerometerData?.acceleration.z)!)
+                    if cont > 0.1 {
+                        self.lastupdated = NSDate()
+                        var velocity: Float = abs(accelX + accelY + accelZ - self.lastX - self.lastY - self.lastZ)
+                        var accel: Float = velocity/Float(cont)
+                        self.lastX = accelX
+                        self.lastY = accelY
+                        self.lastZ = accelZ
+                        self.changeinVar = velocity
+                        
+                        if accel != 0 {
+                            DispatchQueue.main.async {
+                                self.typeOfPassLabel.text = String(accel)
+                            }
+                            if accel > 9 {
+                                self.hasSwung = true
+                                self.grabFlag = false
+//                                print(self.hasSwung)
+                            }
+                        
+                        }
+                        
+                    }
                 }
             }
         }
